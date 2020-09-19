cb869284132a0aacbce205e0f6c08b32b521324d
Nikolay Sorokin <sorokinnikolay88@gmail.com>
2020-09-19 12:50:52 +0300

    changes required for deployment to heroku

--- first/settings.py
+++ first/settings.py
@@ -94,12 +94,16 @@ WSGI_APPLICATION = 'first.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
